91d7c8e747c0eaaf2183f7345f0ea64ace74b8bf
vbazanella <vinicius.bazanella1@gmail.com>
2020-06-20 20:28:27 -0300

    seila

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 
-const hex = fs.readFileSync('./input.txt').toString()
+const hex = fs.readFileSync('./addresses.txt').toString()
     .split('\n')
     .join(' ')
     .split(', ')
     .map(e => parseInt(e, 16))
 
-const bin  = hex.map(e => e.toString(2).padStart(16, "0"))
+const bin = hex.map(e => e.toString(2).padStart(16, "0"))
 
-
-
+console.log(bin)
