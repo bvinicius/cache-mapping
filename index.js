e1f487fa48469b4f43fd05a3dc5d0a3fb5a5bdc2
vbazanella <vinicius.bazanella1@gmail.com>
2020-06-20 12:49:24 -0300

    Implemented hex to binary conversion

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const hex = fs.readFileSync('./input.txt').toString()
     .split('\n')
     .join(' ')
     .split(', ')
+    .map(e => parseInt(e, 16))
+
+const bin  = hex.map(e => e.toString(2).padStart(16, "0"))
+
+
+
