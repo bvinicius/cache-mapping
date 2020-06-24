7a0d95471026cf86b842e0615d79102bf4a37fd3
vbazanella <vinicius.bazanella1@gmail.com>
2020-06-24 18:19:43 -0300

    implemented miss and hit logic

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { strict } = require('assert');
 
 const hex = fs.readFileSync('./addresses.txt').toString()
     .split('\n')
@@ -13,4 +14,64 @@ function possibilities(len) {
     return [...Array(size).keys()].map(e => e.toString(2).padStart(len, "0"))
 }
 
-console.log(possibilities(4))
+function directMapping(_binfile, _tag, _lines, _words, _bitwise) {
+    fs.writeFileSync('gabarito.txt', '')
+    const bin = fs.readFileSync(_binfile).toString().split('\n')
+ 
+    const struct = {}
+    possibilities(_lines).forEach(e => {
+        Object.assign(struct, {[e]: {
+            tag: "",
+            data: []
+        }})
+    })
+
+    let miss = 0, hit = 0
+    bin.forEach(e => {
+        const line = e.slice(_tag, _tag + _lines)
+        const lineTag = struct[line].tag
+        const addressTag = e.slice(0, _tag)
+        
+        console.log(lineTag)
+        console.log(addressTag)
+
+        if (lineTag) {
+            if (lineTag == addressTag) {
+                console.log('AQUIIIII')
+                if (struct[line].data.includes(e)) {
+                    hit ++
+                } else {
+                    miss ++
+                    struct[line].data = possibilities(_words).map(w => addressTag.concat(line).concat(w).concat('0'))
+                    struct[line].tag = addressTag
+                }
+            } else {
+                miss ++
+                struct[line].data = possibilities(_words).map(w => addressTag.concat(line).concat(w).concat('0'))
+                struct[line].tag = addressTag
+            }
+        } else {
+            miss ++
+            struct[line].data = possibilities(_words).map(w => addressTag.concat(line).concat(w).concat('0'))
+            struct[line].tag = addressTag
+        }
+    })
+
+    return {
+        miss: miss,
+        hit: hit,
+        total: miss + hit
+    }
+}
+
+console.log(directMapping('bin.txt', 9, 3, 3, 1))
+
+//olha o próximo endereço
+    //ve se a linha do endereço ja tem coisa dentro
+    //se tiver, olha a tag
+        //se a tag for a mesma
+            //procura na linha se aquele endereço já está lá
+                //se estiver, HIT
+                //se não, MISS e aquela linha é preenchida com os novos valores de acordo com o endereço
+        //se não for a mesma, MISS
+    //se não tiver, MISS e aquela linha é preenchida com os novos valores de acordo com o endereço
